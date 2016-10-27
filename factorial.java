a4e767d7ca099730c08774e61bd77b018341d870
neena <neena.augustine@experioglobal.com>
2016-10-27 13:03:35 +0530

    change

---
+++ factorial.java
@@ -0,0 +1,15 @@
+import java.io.*;
+public static void main()
+{
+     int i,fact=1;
+     System.out.println("enter the number");
+     DataInputstream in=new DataInputstream(system.in);
+     n=in.nextInt(in);
+     for(i=n;i>0;i--)
+       {
+         fact=fact*i;
+       }
+     System.out.println("factorial the number",fact);
+     
+}
+
