f7debd432fdc75f15d8a8b2458d84107c3505ec9
Experionneena <neena.augustine@experionglobal.com>
2016-10-27 14:32:45 +0530

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -4,7 +4,7 @@ public static void main()
      int i,fact=1;
      System.out.println("enter the number");
      DataInputstream in=new DataInputstream(system.in);
-     n=in.nextInt(in);
+     int n=in.nextInt(in);
      for(i=n;i>0;i--)
        {
          fact=fact*i;
